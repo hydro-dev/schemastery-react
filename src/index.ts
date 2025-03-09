efd7221d56f6c881b4039d03b6b672b3753ad443
undefined <i@undefined.moe>
2025-03-09 21:42:31 +0800

    fix types

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ import 'element-plus/dist/index.css';
 import ElementPlus from 'element-plus';
 import form from 'schemastery-vue';
 import { createI18n } from 'vue-i18n';
-import React from 'react';
+import { createElement } from 'react';
 import Schema from 'schemastery';
 import { applyPureReactInVue, applyVueInReact, createReactMissVue } from 'veaury';
 
 const KForm = applyVueInReact(form.Form) as any;
 
-export type MarkdownRenderer = (source: string) => React.ReactNode;
+export type MarkdownRenderer = (props: { source: string }) => React.ReactNode;
 
 export function createSchemasteryReact({
   Markdown, setupVue, locale,
 }: {
-  Markdown: MarkdownRenderer, setupVue(app: any): void, locale: string
+  Markdown: MarkdownRenderer,
+  setupVue?: (app: any) => void,
+  locale?: string,
 }) {
   const [, ReactMissVue] = createReactMissVue({
     useVueInjection() {
@@ -28,14 +30,14 @@ export function createSchemasteryReact({
       }));
       app.use(ElementPlus);
       app.component('k-markdown', applyPureReactInVue(Markdown));
-      setupVue(app);
+      setupVue?.(app);
     },
   });
 
   function App({
     schema, initial, value, onChange, ...props
   }: { schema: Schema<any>, initial: any, value: any, onChange: (value: any) => void } & Partial<React.HTMLAttributes<HTMLDivElement>>) {
-    return React.createElement(ReactMissVue, {}, React.createElement(KForm, { ...props, schema, initial, 'v-model': [value, onChange] }));
+    return createElement(ReactMissVue, {}, createElement(KForm, { ...props, schema, initial, 'v-model': [value, onChange] }));
   }
 
   return App;
